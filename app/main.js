6cc0ec741d27aab984f4809770a1e6de24f7d1f2
Vitenskap <viniciusalvesms757@gmail.com>
2024-02-21 20:12:26 -0300

    Comit semi-final

--- app/main.js
+++ app/main.js
@@ -1,9 +1,12 @@
+//==Declaração de variáveis==
 let inputTask = document.querySelector('#inputTask')
 let button = document.querySelector('#createButton')
 let taskList = document.querySelector('#taskList')
 
 let listaDeTarefas = []
 
+
+//==Criação de Tasks==
 function criarTask() {
     listaDeTarefas.push({
         task: inputTask.value,
@@ -14,24 +17,6 @@ function criarTask() {
     setTimeout(() => {
         esconderAlerta()
     }, 1000)
-}
-
-function carregarStorage() {
-    if (localStorage.getItem('Lista')) {
-        let recString = localStorage.getItem('Lista')
-        listaDeTarefas = JSON.parse(recString)
-    let novoItem = ''
-    listaDeTarefas.forEach((el, pos) => {
-        novoItem += `<div id="taskBox" class="taskBox">
-        <input class="iconify check ${el.isChecked && "done"}" data-icon="mdi-check" onClick="concluirTask(${pos})" id="check">
-
-        <p id="task" class="task">${el.task}</p>
-
-        <button onClick="deletarTask(${pos})" type="button" class="iconify trash" data-icon="mdi-trash"></button>
-    </div>`
-        taskList.innerHTML = novoItem
-    })
-    }
 }
 
 function mostrarAlerta() {
@@ -50,28 +35,53 @@ function mostrarTasks() {
     listaDeTarefas.forEach((el, pos) => {
         novoItem += `<div id="taskBox" class="taskBox">
         <input class="iconify check ${el.isChecked && "done"}" data-icon="mdi-check" onClick="concluirTask(${pos})" id="check">
-
+        
         <p id="task" class="task">${el.task}</p>
-
+        
         <button onClick="deletarTask(${pos})" type="button" class="iconify trash" data-icon="mdi-trash"></button>
-    </div>`
+        </div>`
     })
     taskList.innerHTML = novoItem
     let armazenadas = JSON.stringify(listaDeTarefas)
     localStorage.setItem("Lista", armazenadas)
+    carregarStorage()
 }
 
+
+
+//==Eventos manipulação de Tasks==
 function concluirTask(pos){
     listaDeTarefas[pos].isChecked = !listaDeTarefas[pos].isChecked
     mostrarTasks()
-    carregarStorage()
 }
 
 function deletarTask(pos) {
     listaDeTarefas.splice(pos, 1)
     mostrarTasks()
-    carregarStorage()
 }
-
 button.addEventListener('click', criarTask)
+
+
+
+//==Carregamento de localStorage==
+function carregarStorage() {
+    if (localStorage.getItem('Lista')) {
+        let recString = localStorage.getItem('Lista')
+        listaDeTarefas = JSON.parse(recString)
+    let novoItem = ''
+    listaDeTarefas.forEach((el, pos) => {
+        novoItem += `<div id="taskBox" class="taskBox">
+        <input class="iconify check ${el.isChecked && "done"}" data-icon="mdi-check" onClick="concluirTask(${pos})" id="check">
+
+        <p id="task" class="task">${el.task}</p>
+
+        <button onClick="deletarTask(${pos})" type="button" class="iconify trash" data-icon="mdi-trash"></button>
+    </div>`
+        taskList.innerHTML = novoItem
+    })
+    }
+}
 document.body.addEventListener('load', carregarStorage)
+
+
+
